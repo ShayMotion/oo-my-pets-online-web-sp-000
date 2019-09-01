0e31456169c4604e924fdd50a5eece738f0c0cee
Shayan Mohsin <shaymotionfilms@gmail.com>
2019-09-01 19:40:54 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,75 +1,68 @@
-class Owner 
-  # code goes here		attr_accessor :name, :pets
-end 		attr_reader :species 
+class Owner
+  # code goes here	  attr_accessor :pets, :name
+end 	  attr_reader :species
 
+   @@all = []
 
- 	OWNERS = []
+   def initialize(species)
+    @species = species
+    @@all << self
+    @pets = { :fishes => [], :dogs => [], :cats => [] }
+  end
 
+   def self.all
+    @@all
+  end
 
- 	def initialize(species)
-		@species = species 
-		@pets = {:fishes => [], :cats => [], :dogs => []}
-		OWNERS << self
-	end
+   def self.reset_all
+    @@all.clear
+  end
 
- 	def say_species 
-		"I am a #{species}."
-	end
+   def self.count 
+    @@all.size
+  end
 
- 	def self.all 
-		OWNERS
-	end
+   def say_species
+    "I am a #{species}."
+  end
 
- 	def self.reset_all
-		OWNERS.clear 
-	end 
+   def buy_fish(name)
+    fish = Fish.new(name)
+    pets[:fishes] << fish
+  end
 
- 	def self.count 
-		OWNERS.size 
-	end
+   def buy_cat(name)
+    cat = Cat.new(name)
+    pets[:cats] << cat
+  end
 
- 	def buy_fish(name) 
-		pets[:fishes] << Fish.new(name)
-	end
+   def buy_dog(name)
+    dog = Dog.new(name)
+    pets[:dogs] << dog
+  end
 
- 	def buy_cat(name)
-		pets[:cats] << Cat.new(name)
-	end 
+   def walk_dogs
+    pets[:dogs].map {|dog| dog.mood = "happy"}
 
- 	def buy_dog(name)
-		pets[:dogs] << Dog.new(name)
-	end 
+   end
 
- 	def walk_dogs 
-		pets[:dogs].each do |dog|
-			dog.mood = "happy"
-		end
-	end
+   def play_with_cats
+    pets[:cats].map {|cat| cat.mood = "happy"}
+  end
 
- 	def play_with_cats
-		pets[:cats].each do |cat|
-			cat.mood = "happy"
-		end
-	end
+   def feed_fish
+    pets[:fishes].map {|fish| fish.mood = "happy"}
+  end
 
- 	def feed_fish
-		pets[:fishes].each do |fish|
-			fish.mood = "happy"
-		end
-	end
+   def sell_pets
+    pets.each do |type, pets|
+      pets.map {|pet| pet.mood = "nervous"}
+    end
+    pets.clear
+  end
 
- 	def sell_pets
-		pets.each do |species, animals|
-		 animals.each do |animal|
-		 	animal.mood = "nervous"
-		 end
-		 animals.clear
-		end 
-	end 
-
- 	def list_pets 
-	"I have #{pets[:fishes].count} fish, #{pets[:dogs].count} dog(s), and #{pets[:cats].count} cat(s)."
-	end 
-
+   def list_pets
+    "I have #{pets[:fishes].size} fish, #{pets[:dogs].size} dog(s), and #{pets[:cats].size} cat(s)."
+  end
 
  end
